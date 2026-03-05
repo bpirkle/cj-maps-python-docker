38432f3c4542ae53bf43178674eec92b0e576eb4
bpirkle <bpirkle@wikimedia.org>
2026-03-05 12:53:23 -0600

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cj-maps-python-docker
 Fiddling around with Docker and Python for a maps project
+
+http://localhost:8089
+docker compose up -d --build
+docker stop
+docker down
---
+++ app.py
@@ -0,0 +1,26 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Mabel!'
+
+@app.route('/mabel')
+def mabel():
+        return 'Mabel<br><b>Mabel</b><br><em>Mabel!</em><br>Good girl.'
+
+@app.route('/down')
+def here_mabel():
+    file_path = 'down.html'
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8089)
